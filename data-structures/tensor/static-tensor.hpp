da7f689f779a4e1739dbbb3d901774cc22fce8a6
Joseph Amirtharaj <epicjoe128@gmail.com>
2022-02-21 15:52:05 +0530

    add static-tensor.hpp

---
+++ data-structures/tensor/static-tensor.hpp
@@ -0,0 +1,19 @@
+#pragma
+
+#include <array>
+
+template<typename T, int N, int...args>
+struct static_tensor : std::array<static_tensor<T, args...>, N> {
+    static_tensor() : std::array<static_tensor<T, args...>, N>() {}
+
+    void fill(const T &val) {
+        for (int i = 0; i < N; i++) {
+            (*this)[i].fill(val);
+        }
+    }
+};
+
+template<typename T, int N>
+struct static_tensor<T, N> : std::array<T, N> {
+    static_tensor() : std::array<T, N>() {}
+};
