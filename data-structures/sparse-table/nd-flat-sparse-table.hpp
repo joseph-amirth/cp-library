fce9a26032c191d1805b4112c7db7892b5486c0b
Joseph Amirtharaj <epicjoe128@gmail.com>
2022-02-21 20:47:06 +0530

    update nd-flat-sparse-table.hpp

--- data-structures/sparse-table/nd-flat-sparse-table.hpp
+++ data-structures/sparse-table/nd-flat-sparse-table.hpp
@@ -1,6 +1,6 @@
-#pragma
+#pragma once
 
-#include "tensor/flat-tensor.hpp"
+#include "../tensor/flat-tensor.hpp"
 
 template <typename T, int D = 1, typename F = std::function<T(const T&, const T&)>>
 struct nd_flat_sparse_table {
@@ -21,7 +21,7 @@ struct nd_flat_sparse_table {
         mat = flat_tensor<T, 2 * D>(mat_shape);
         auto build = [&](int t_offset, int mat_offset, int d, auto &&self) -> void {
             if (d == D) {
-                mat[mat_offset] = T(t[t_offset]);
+                mat.data[mat_offset] = T(t.data[t_offset]);
             } else {
                 int lg = mat.shape[2 * d], n = mat.shape[2 * d + 1];
                 for (int i = 0; i < n; i++) {
@@ -33,7 +33,7 @@ struct nd_flat_sparse_table {
                         auto a_offset = mat_offset + (j - 1) * mat.strides[2 * d] + i * mat.strides[2 * d + 1];
                         auto b_offset = mat_offset + (j - 1) * mat.strides[2 * d] + (i + (1 << (j - 1))) * mat.strides[2 * d + 1];
                         for (int k = 0; k < mat.strides[2 * d + 1]; k++) {
-                            mat[result_offset + k] = f(mat[a_offset + k], mat[b_offset + k]);
+                            mat.data[result_offset + k] = f(mat.data[a_offset + k], mat.data[b_offset + k]);
                         }
                     }
                 }
@@ -43,15 +43,19 @@ struct nd_flat_sparse_table {
     }
 
     T query(const std::initializer_list<std::pair<int, int>> &range) {
+        int i = 0;
+        for (auto [l, r] : range) {
+            assert(0 <= l && l <= r && r < mat.shape[2 * i + 1]);
+            i += 1;
+        }
         auto recurse = [&](auto it, int d, int mat_offset, auto &&self) -> T {
             if (it != range.end()) {
                 auto [l, r] = *it;
-                assert(0 <= l && l <= r && r < mat.shape[2 * d + 1]);
                 int j = 32 - __builtin_clz(r - l + 1) - 1;
                 return f(self(std::next(it), d + 1, mat_offset + j * mat.strides[2 * d] + l * mat.strides[2 * d + 1], self),
                          self(std::next(it), d + 1, mat_offset + j * mat.strides[2 * d] + (r - (1 << j) + 1) * mat.strides[2 * d + 1], self));
             } else {
-                return mat[mat_offset];
+                return mat.data[mat_offset];
             }
         };
         return recurse(range.begin(), 0, 0, recurse);
