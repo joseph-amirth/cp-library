f7e12e3c05b1a61a0c0363df070fa9e7284b015c
joseph-amirth <epicjoe128@gmail.com>
2022-11-05 01:48:41 +0530

    update and add a bunch of stuff

--- newgraphs/primitives/get-vertices-on-path.hpp
+++ newgraphs/primitives/get-vertices-on-path.hpp
@@ -7,21 +7,19 @@ namespace graphs {
 template <typename Graph>
 std::vector<int> get_vertices_on_path(const Graph &g, int s, int t, auto &&p) {
     std::vector<int> path;
-    for (int u = t; u != s; ) {
+    for (int u = t; u != s;) {
         path.push_back(u);
-        const auto &e = g.edges[p(u)];
-        u ^= e.u ^ e.v;
+        if constexpr (!std::is_same_v<std::decay_t<decltype(p)>, std::vector<int>>) {
+            const auto &e = g.edges[p(u)];
+            u ^= e.u ^ e.v;
+        } else {
+            const auto &e = g.edges[p[u]];
+            u ^= e.u ^ e.v;
+        }
     }
     path.push_back(s);
     std::reverse(path.begin(), path.end());
     return path;
-}
-
-template <typename Graph>
-std::vector<int> get_vertices_on_path(const Graph &g, int s, int t, const std::vector<int> &p) {
-    return get_vertices_on_path(g, s, t, [&p](int u) {
-        return p[u];
-    });
 }
 
 }
