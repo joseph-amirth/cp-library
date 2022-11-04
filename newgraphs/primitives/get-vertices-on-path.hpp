74a4df23bedbb8f0ef752f4497164cb9909b2156
joseph-amirth <epicjoe128@gmail.com>
2022-11-05 01:49:20 +0530

    update and add a bunch of stuff

--- newgraphs/primitives/get-vertices-on-path.hpp
+++ newgraphs/primitives/get-vertices-on-path.hpp
@@ -9,11 +9,11 @@ std::vector<int> get_vertices_on_path(const Graph &g, int s, int t, auto &&p) {
     std::vector<int> path;
     for (int u = t; u != s;) {
         path.push_back(u);
-        if constexpr (!std::is_same_v<std::decay_t<decltype(p)>, std::vector<int>>) {
-            const auto &e = g.edges[p(u)];
+        if constexpr (std::is_same_v<std::decay_t<decltype(p)>, std::vector<int>>) {
+            const auto &e = g.edges[p[u]];
             u ^= e.u ^ e.v;
         } else {
-            const auto &e = g.edges[p[u]];
+            const auto &e = g.edges[p(u)];
             u ^= e.u ^ e.v;
         }
     }
